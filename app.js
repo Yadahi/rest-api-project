c101d303e79c172c57443b24cc05381fc735511a
Viliam <vatvil@gmail.com>
2024-04-23 20:40:55 +0200

    create feed controller for get and create posts

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const feeRoutes = require("./routes/feed");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use("/feed", feeRoutes);
 
--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,3 +1,26 @@
-const getPosts = (req, res, next) => {};
+const getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content: "This is my first post",
+      },
+    ],
+  });
+};
 
-module.exports = { getPosts };
+const createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post Created",
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+    },
+  });
+};
+
+module.exports = { getPosts, createPost };
