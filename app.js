1c4ec35c342f2faa4550673b8474963c1bef0e45
Viliam <vatvil@gmail.com>
2024-04-23 21:03:58 +0200

    solving cors error

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  /** This sets the value of the Access-Control-Allow-Origin
+   * response header to *, which allows the resource to be
+   * accessed by any domain. */
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  /** This sets the value of the Access-Control-Allow-Methods
+   * response header to specify the HTTP methods that are allowed
+   * when accessing the resource. In this case, it allows GET, POST, PUT, PATCH, and DELETE methods.*/
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+
+  /** This sets the value of the Access-Control-Allow-Headers
+   * response header to specify the headers that are allowed in a
+   * preflight request. In this case, it allows Content-Type and Authorization headers. */
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feeRoutes);
 
 app.listen(8080, () => console.log("Server running on port 8080"));
