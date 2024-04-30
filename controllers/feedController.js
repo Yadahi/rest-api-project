875691f94be5901026cbe4309e2297de989f8772
Viliam <vatvil@gmail.com>
2024-04-30 21:57:01 +0200

    updating post

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const post = require("../models/post");
 
 const getPosts = (req, res, next) => {
   // Fetch posts from db
@@ -84,4 +88,66 @@ const getPost = (req, res, next) => {
     });
 };
 
-module.exports = { getPosts, createPost, getPost };
+const updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post updated", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+/**
+ * Deletes the image file at the specified file path.
+ *
+ * @param {string} filePath - The path to the image file to be deleted.
+ * @return {undefined} This function does not return a value.
+ */
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
+
+module.exports = { getPosts, createPost, getPost, updatePost };
