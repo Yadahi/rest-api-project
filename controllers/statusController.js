304450b88dbd413aa6dafe42ac078da0de008e9c
Viliam <vatvil@gmail.com>
2024-05-10 21:01:04 +0200

    refactor auth and status controller's then() to asyn/await

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 
 const { validationResult } = require("express-validator");
 
-const updateStatus = (req, res, next) => {
+const updateStatus = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed");
@@ -10,44 +10,43 @@ const updateStatus = (req, res, next) => {
     error.data = errors.array();
     throw error;
   }
-  User.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        const error = new Error("Could not find user");
-        error.statusCode = 404;
-        throw error;
-      }
+  try {
+    const user = await User.findById(req.userId);
 
-      user.status = req.body.status;
-      return user.save();
-    })
-    .then((result) => {
-      res.status(200).json({ message: "Status changed." });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+    if (!user) {
+      const error = new Error("Could not find user");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    user.status = req.body.status;
+    await user.save();
+
+    res.status(200).json({ message: "Status changed." });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-const getStatus = (req, res, next) => {
-  User.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        const error = new Error("Could not find user");
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({ status: user.status });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+const getStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      const error = new Error("Could not find user");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ status: user.status });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 module.exports = { updateStatus, getStatus };
