ee9b4156524bffb3dc9c33f0a736c5ab08be6879
Viliam <vatvil@gmail.com>
2024-05-14 21:41:45 +0200

    cereate mutation in schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,19 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 module.exports = {
-  hello: () => {
-    return {
-      text: "hello world",
-      views: 1245,
-    };
+  createUser: async function ({ userInput }, req) {
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      throw new Error("User exists already.");
+    }
+    const hashedPassword = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPassword,
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
 };
