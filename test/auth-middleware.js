c7b5025e82a4c051fae25a04b20334f5920125eb
Viliam <vatvil@gmail.com>
2024-05-27 20:23:10 +0200

    using sinon stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,5 +1,8 @@
 const expect = require("chai").expect;
 const authMiddleware = require("../middleware/is-auth");
+const sinon = require("sinon");
+
+const jwt = require("jsonwebtoken");
 
 describe("Auth middleware", function () {
   it("should throw an error in no auth header", function () {
@@ -8,7 +11,7 @@ describe("Auth middleware", function () {
         return null;
       },
     };
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+    expect(() => authMiddleware(req, {}, () => {})).to.throw(
       "Not authenticated."
     );
   });
@@ -19,6 +22,32 @@ describe("Auth middleware", function () {
         return "xyz";
       },
     };
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    expect(() => authMiddleware(req, {}, () => {})).to.throw();
+  });
+
+  it("should throw an error if the token can not be verified", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyz";
+      },
+    };
+    expect(() => authMiddleware(req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId after decoding the token", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyz";
+      },
+    };
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({
+      userId: "abc",
+    });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    // expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
   });
 });
