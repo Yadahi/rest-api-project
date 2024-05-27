299ad59999ceeac747961f8943b8053415c64d4b
Viliam <vatvil@gmail.com>
2024-05-27 19:46:10 +0200

    testing auth middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,12 +2,13 @@ const jwt = require("jsonwebtoken");
 const credentials = require("../config/credentials");
 
 module.exports = (req, res, next) => {
-  if (req.get("Authorization") === undefined) {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
     const error = new Error("Not authenticated.");
     error.statusCode = 401;
     throw error;
   }
-  const token = req.get("Authorization").split(" ")[1];
+  const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, credentials.jwtSecret);
