defaac915f1b12a8d0133239c945026f1737feb5
Viliam <vatvil@gmail.com>
2024-05-29 19:21:12 +0200

    setting up testing database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,8 +1,11 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/authController");
+const StatusController = require("../controllers/statusController");
+const credentials = require("../config/credentials");
 
 describe("Auth controller - login", function () {
   it("should throw an error with status 500 if accessing the database fails", function (done) {
@@ -24,5 +27,46 @@ describe("Auth controller - login", function () {
     });
 
     User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", function (done) {
+    mongoose
+      .connect(
+        `${credentials.MONGO_SCHEME}://${credentials.MONGO_USER}:${credentials.MONGO_PASSWORD}@${credentials.MONGO_HOSTNAME}/test-messages?retryWrites=true`
+      )
+      .then((result) => {
+        const user = new User({
+          email: "a@a.com",
+          password: "12345",
+          name: "test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a",
+        });
+        return user.save();
+      })
+      .then(() => {
+        const req = {
+          userId: "5c0f66b979af55031b34728a",
+        };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+        StatusController.getStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("I am new");
+          done();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 });
